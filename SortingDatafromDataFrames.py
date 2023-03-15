6732b59990aae9760d159c663662f8a0d1b3e178
frankovat <45593012+frankovat@users.noreply.github.com>
2023-03-15 19:58:41 +0900

    Update SortingDatafromDataFrames.py

--- SortingDatafromDataFrames.py
+++ SortingDatafromDataFrames.py
@@ -1,22 +1,28 @@
 import numpy as np
 import pandas as pd
 
-TbedDec=pd.read_csv("/Users/terezafrankova/Desktop/Cluster/Cafe/maDec.tsv",sep="\t")
-TbedDec=TbedDec.drop('First',axis=1)
+TbedDec=pd.read_csv("/Users/terezafrankova/Desktop/Cluster/Cafe/maDec.tsv",sep="\t") #import file with gene and function (olfactory receptor)
+TbedDec=TbedDec.drop('gene_function',axis=1) #drop the second column, 'gene_function' is the name of the second column, use your name!
 TbedDec=TbedDec.values
 TbedDec=TbedDec.flatten()
 
-Orthogroups=pd.read_csv("/Users/terezafrankova/Downloads/Orthogroups.csv",sep='\t')
-Tbed=Orthogroups[['Orthogroups','Taur_pep']].copy()
-Tbed=Tbed.dropna()
-Tbed=Tbed.set_index(['Orthogroups']).apply(lambda x: x.str.split(', ').explode()).reset_index()
+Orthogroups=pd.read_csv("/Users/terezafrankova/Downloads/Orthogroups.csv",sep='\t') #upload the peptidic file of your species
+#Tbed=Orthogroups[['Orthogroups','Taur_pep']].copy() - you will not need this
+#Tbed=Tbed.dropna()
+#Tbed=Tbed.set_index(['Orthogroups']).apply(lambda x: x.str.split(', ').explode()).reset_index()
+#HERE YOU WILL NEED TO CREATE TWO COLUMNS, ONE FROM THE GENES AND SECOND FROM THE SEQUENCES!!! example:
+#     q54224.2              AOFHEBSMDLFJSNSBCJCDKJSBDD
+#     q3736.13              URHSXCKFPSJFHGGCZSJAKLWOHF
+#then you will name the column one as gene and column two as sequence
 
-tbedMinus=Tbed[Tbed['Orthogroups'].isin(TbedDec)]
+tbedMinus=Tbed[Tbed['Orthogroups'].isin(TbedDec)] #here you will ask if the column gene is in the created TbedDec
 tbedMinus=tbedMinus.drop('Orthogroups',axis=1)
 tbedMinus=tbedMinus.values
 tbedMinus=tbedMinus.flatten()
-tbedMinus=[s.replace('ta','g') for s in tbedMinus]
+#tbedMinus=[s.replace('ta','g') for s in tbedMinus] #changes ta1.12 to g1.12, you don't need to use it!
 
+### AND THIS SHOULD BE ENOUGHT TO GET THE SEQUENCES FROM IT!!!
+==========================================================================
 tbedGF=pd.read_csv("/Users/terezafrankova/Downloads/TaurGF.csv", sep='\t')
 tbedGF=tbedGF.dropna()
 tbedGF.drop(tbedGF.loc[tbedGF['Function']=='-'].index, inplace=True)
